475532c84cb34b76a0ae52e2f5ee92a8dfb4b855
qianfeng <qianfeng@piao.local>
2014-10-16 14:38:22 +0800

    re

---
+++ 32145678/eqwViewController.m
@@ -0,0 +1,49 @@
+//
+//  eqwViewController.m
+//  32145678
+//
+//  Created by qianfeng on 14-10-16.
+//  Copyright (c) 2014年 行知合一. All rights reserved.
+//
+
+#import "eqwViewController.h"
+
+@interface eqwViewController ()
+
+@end
+
+@implementation eqwViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
